8f325efd8e337b2eb0c512317724218183c294ea
Alex <alexandre.dutay@outlook.fr>
2023-01-07 09:57:01 +0100

    fix css

--- src/components/layout/header/navigation/styles.tsx
+++ src/components/layout/header/navigation/styles.tsx
@@ -15,8 +15,8 @@ export const NavList = styled.ul`
   font-size: 1.25rem;
   list-style: none;
   overflow-x: auto;
-  -webkit-touch-callout: none; /* iOS Safari */
-  -webkit-user-select: none; /* Safari */
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
   user-select: none;
