11acd42235e21dc5a0ed60e3472e13397c93b167
Alex <alexandre.dutay@outlook.fr>
2023-01-09 13:29:47 +0100

    adding shouldForwardProp to styled Stack

--- src/components/stack/styles.tsx
+++ src/components/stack/styles.tsx
@@ -1,7 +1,10 @@
 import type { StackProps } from '../../types';
 import styled from 'styled-components';
 
-export const Stack = styled.div<StackProps>`
+export const Stack = styled.div.withConfig({
+  shouldForwardProp: (prop, defaultValidatorFn) =>
+    !['direction', 'space'].includes(prop) && defaultValidatorFn(prop),
+})<StackProps>`
   & > *:not(:last-child) {
     ${({ direction, space, theme }) =>
       direction === 'vertical'
